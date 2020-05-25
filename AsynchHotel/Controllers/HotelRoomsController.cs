8290d1c380754c4db05423e6d008550ef35ddcb9
Zscoob <james.zobian@gmail.com>
2020-05-25 15:14:17 -0500

    controllers added

---
+++ AsynchHotel/Controllers/HotelRoomsController.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AsynchHotel.Controllers
+{
+    public class Class HotelRoomsController : Controller
+    {
+        public IActionResult Index()
+         {
+        return View();
+         }
+    }
+}
+
